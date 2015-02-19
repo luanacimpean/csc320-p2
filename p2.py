d7980383d74c2aa15d8f78f9eaebffb90a89dd48
luanacimpean <luanacimpean@gmail.com>
2015-02-19 18:50:40 -0500

    Update p2.py

--- p2.py
+++ p2.py
@@ -157,7 +157,9 @@ if __name__ == "__main__":
         k += 1
         # finding a negative pixel
         # Randomly select stroke center
-        cntr = np.floor(np.random.rand(2,1).flatten() * np.array([len(np.where(canvas == -1)[0]), len(np.where(canvas == -1)[1])])) + 1
+        rando = randint(0,len(np.where(canvas == -1)[1]))
+        cntr = np.array([np.where(canvas == -1)[0][rando], np.where(canvas == -1)[1][rando]])
+        
         cntr = np.amin(np.vstack((cntr, np.array([sizeIm[1], sizeIm[0]]))), axis=0)
         # Grab colour from image at center position of the stroke.
         colour = np.reshape(imRGB[cntr[1]-1, cntr[0]-1, :],(3,1))
