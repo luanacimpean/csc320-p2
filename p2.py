1c6d2fc4b95327347cebc839db0f062d9f5f5abb
luanacimpean <luanacimpean@gmail.com>
2015-02-21 17:41:34 -0500

    Update p2.py

--- p2.py
+++ p2.py
@@ -172,10 +172,40 @@ if __name__ == "__main__":
         colour = np.reshape(imRGB[cntr[1]-1, cntr[0]-1, :],(3,1))
         # Add the stroke to the canvas
         nx, ny = (sizeIm[1], sizeIm[0])
-        length1, length2 = (halfLen, halfLen)        
-        canvas = paintStroke(canvas, x, y, cntr - delta * length2, cntr + delta * length1, colour, rad)
-        #print imRGB[cntr[1]-1, cntr[0]-1, :], canvas[cntr[1]-1, cntr[0]-1, :]
-        print 'stroke', k
+        length1, length2 = (halfLen, halfLen) 
+
+        if (cannied[cntr[1]][cntr[0]] == 1):
+            # Grab colour from image at center position of the stroke.
+            colour = np.reshape(imRGB[cntr[1]-1, cntr[0]-1, :],(3,1))
+            # Add the stroke to the canvas
+            nx, ny = (sizeIm[1], sizeIm[0])
+            # length1, length2 = (halfLen, halfLen)        
+            canvas = paintStroke(canvas, x, y, cntr, cntr, colour, rad)
+        
+        mult1 = 1
+        p1 = cntr - [int(i) for i in delta]
+        while ((mult1 < length2) and (cannied[p1[1]][p1[0]] != 1)):
+            p1 = cntr - [int(i)*mult1 for i in delta]
+            canvas = paintStroke(canvas, x, y, p1, cntr, colour, rad)
+            mult1 = mult1 + 1
+        
+        mult2 = 1
+        p2 = cntr + [int(i) for i in delta]
+        while ((mult2 < length1) and (cannied[p2[1]][p2[0]] != 1)):
+            p2 = cntr + [int(i)*mult2 for i in delta]
+            canvas = paintStroke(canvas, x, y, cntr, p2, colour, rad)
+            mult2 = mult2 + 1
+
+
+
+#        # Grab colour from image at center position of the stroke.
+#        colour = np.reshape(imRGB[cntr[1]-1, cntr[0]-1, :],(3,1))
+#        # Add the stroke to the canvas
+#        nx, ny = (sizeIm[1], sizeIm[0])
+#        length1, length2 = (halfLen, halfLen)        
+#        canvas = paintStroke(canvas, x, y, cntr - delta * length2, cntr + delta * length1, colour, rad)
+#        #print imRGB[cntr[1]-1, cntr[0]-1, :], canvas[cntr[1]-1, cntr[0]-1, :]
+#        print 'stroke', k
         unpainted = np.where (canvas == -1)
         
     print "done!"
